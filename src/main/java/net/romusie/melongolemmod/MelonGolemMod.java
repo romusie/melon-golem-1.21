3c45390afbd97f2c830f633db98c1ace908e8ef1
romusie <bartlomiej.jagielski.155@gmail.com>
2025-04-20 10:48:13 +0200

    Initial commit

--- src/main/java/net/romusie/melongolemmod/MelonGolemMod.java
+++ src/main/java/net/romusie/melongolemmod/MelonGolemMod.java
@@ -10,7 +10,7 @@ import net.romusie.melongolemmod.event.MelonStripEventHandler;
 import net.romusie.melongolemmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//test
 public class MelonGolemMod implements ModInitializer {
 	public static final String MOD_ID = "melongolem-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
